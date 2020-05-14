21b1e1d176256edc501134c39f3cbc3a1e53b831
AndrianJunior <62462515+AndrianJunior@users.noreply.github.com>
2020-05-14 21:14:02 +0300

    Create training2.py
    добавил пример диапазона.range

---
+++ les1/training2.py
@@ -0,0 +1,10 @@
+numbers = list(range(100))#диапазон
+
+numbers = list(range(50,100))#это диапазон от 50 до 100
+
+print(numbers)
+
+numbers2 = list(range(50,100,2))
+print(numbers2)
+# а тут получaеться от 50 до 100 и третья цифра шаг
+#тоесть через сколько
