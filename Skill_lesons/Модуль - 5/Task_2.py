c7107b9df68d4f06e7285b3a8df00b12663cfff7
AndrianJunior <andrian.sol@mail.ru>
2021-06-14 13:01:21 +0300

    исправил

--- Skill_lesons/Модуль - 5/Task_2.py
+++ Skill_lesons/Модуль - 5/Task_2.py
@@ -10,9 +10,9 @@ print('Задача 2. Максимум из трёх чисел')
 num_1 = int(input("Введите первое число:"))
 num_2 = int(input("Введите второе число:"))
 num_3 = int(input("Введите третье число:"))
-if num_1 > num_2:
+if num_1 > num_2 and num_1 > num_3:
     print(num_1)
-elif num_2 > num_3:
+elif num_2 > num_3 and num_2 > num_1:
     print(num_2)
 else:
     print(num_3)
