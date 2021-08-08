73049bc23678e3c1a90f103320506aa856aedd99
AndrianJunior <andrian.sol@mail.ru>
2021-08-08 11:03:36 +0300

    +

---
+++ Skill_lesons/Модуль 8/Task_7.py
@@ -0,0 +1,18 @@
+print('Задача 7. Стипендия')
+
+#Ежемесячная стипендия студента составляет educational_grant руб.,
+# а расходы на проживание превышают стипендию и составляют expenses руб. в месяц.
+# Рост цен ежемесячно увеличивает расходы на 3%, кроме первого месяца.
+#
+# Составьте программу расчета суммы денег,
+# которую необходимо получить у родителей один раз в начале обучения,
+# чтобы можно было прожить учебный год (10 месяцев), используя только эти деньги и стипендию.
+educational_grant, expenses = 10000, 12000
+months = 10
+inflation = 1.03
+total_expenses = expenses
+for _ in range(2, 11):
+    expenses = expenses * inflation
+    total_expenses += expenses
+parents_money = total_expenses - educational_grant * months
+print("Студенту надо попросить",round(parents_money, 2), "рублей")
